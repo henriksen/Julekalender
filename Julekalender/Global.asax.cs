1155e51284752c63db895e9b1307fba101a866de
Glenn F. Henriksen <glenn@henriksen.no>
2013-11-29 09:53:45 +0100

    Started on the winner draw. Added snow when the winner is picked

--- Julekalender/Global.asax.cs
+++ Julekalender/Global.asax.cs
@@ -23,19 +23,50 @@ namespace Julekalender
             {
                 while (true)
                 {
-                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<countDownHub>();
-                    hubContext.Clients.All.upDateCountdown(
-                        (new DateTime(2013, 12, 2, 12, 0, 0) -
-                        DateTime.Now).ToString()
-                        );
-                    Thread.Sleep(1000);
+                    var timeUntilOpening = new DateTime(2013, 11, 29, 9, 3, 0) -
+                                           DateTime.Now;
+                    var msg = timeUntilOpening.Days > 0 ? timeUntilOpening.Days + 
+                        (timeUntilOpening.Days == 1 ? " dag, " : " dager, ") : "";
+                    msg += timeUntilOpening.Hours > 0 ? timeUntilOpening.Hours + 
+                        (timeUntilOpening.Hours == 1 ? " time, " : " timer, ") : "";
+                    msg += timeUntilOpening.Minutes > 0 ? timeUntilOpening.Minutes + 
+                        (timeUntilOpening.Minutes == 1 ? " minutt, " : " minutter, ") : "";
+                    msg += timeUntilOpening.Seconds +
+                        (timeUntilOpening.Seconds == 1 ? " sekund " : " sekunder ");
+
+                    msg += " til pakkeåpning";
+
+                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<CountDownHub>();
+                    hubContext.Clients.All.upDateCountdown(msg);
+                    if (timeUntilOpening.TotalSeconds > 0)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        var names = new string[] {"Per", "Pål", "Espen", "Kari", "Mari"};
+                        int length = names.Length;
+                        Random rnd = new Random();
+                        var target = rnd.Next(50, 100);
+                        for (int i = 0; i < target ; i++)
+                        {
+                            var index = i%length;
+                            hubContext.Clients.All.upDateCountdown(names[index]);
+                            Thread.Sleep(100+i);
+                        }
+                        hubContext.Clients.All.letItSnow();
+
+                        Thread.Sleep(10000);
+                    }
+
+
 
                 }
             });
         }
     }
 
-    public class countDownHub : Hub
+    public class CountDownHub : Hub
     {
     }
 }
